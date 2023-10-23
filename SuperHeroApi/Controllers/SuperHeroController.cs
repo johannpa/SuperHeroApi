0dc23137410c338f1b06cd1022dd9ba010ff59c2
johannpa <johann_paque@hotmail.fr>
2023-10-23 19:17:50 +0200

    Create Post method AddHero in the controller and create a new hero in the list

--- SuperHeroApi/Controllers/SuperHeroController.cs
+++ SuperHeroApi/Controllers/SuperHeroController.cs
@@ -16,12 +16,26 @@ namespace SuperHeroApi.Controllers
                     FirstName = "Peter",
                     LastName = "Parker",
                     Place = "New York City"
+                },
+                new SuperHero {
+                    Id = 2,
+                    Name = "Iron Man",
+                    FirstName = "Tony",
+                    LastName = "Stark",
+                    Place = "Long Island"
                 }
             };
 
         [HttpGet]
         public async Task<ActionResult<List<SuperHero>>> Get()
         {
+            return Ok(heroes);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<SuperHero>>> AddHero(SuperHero hero)
+        {
+            heroes.Add(hero);
             return Ok(heroes);
         }
     }
