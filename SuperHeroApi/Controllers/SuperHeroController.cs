677cb25d97b43884a438e8b4144e83b5b7792df8
johannpa <johann_paque@hotmail.fr>
2023-10-24 20:23:05 +0200

    Changed the Put method, the UpdateHero

--- SuperHeroApi/Controllers/SuperHeroController.cs
+++ SuperHeroApi/Controllers/SuperHeroController.cs
@@ -57,18 +57,20 @@ namespace SuperHeroApi.Controllers
         [HttpPut]
         public async Task<ActionResult<List<SuperHero>>> UpdateHero(SuperHero request)
         {
-            var hero = heroes.Find(h => h.Id == request.Id);
-            if(hero == null)
+            var dbHero = await _context.SuperHeroes.FindAsync(request.Id);
+            if (dbHero == null)
             {
-                return BadRequest("Hero not found");
+                return BadRequest("Hero not found.");
             }
 
-            hero.Name = request.Name;
-            hero.FirstName = request.FirstName;
-            hero.LastName = request.LastName;
-            hero.Place = request.Place;
+            dbHero.Name = request.Name;
+            dbHero.FirstName = request.FirstName;
+            dbHero.LastName = request.LastName;
+            dbHero.Place = request.Place;
 
-            return Ok(heroes);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.SuperHeroes.ToListAsync());
         }
 
 
